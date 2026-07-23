3e11d6ed17aab1af75bbfe98f86a725075a6c587
dauphinem <dauphine.monclar@gmail.com>
2026-07-23 16:00:55 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="grow-static-v1";
+const FILES=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./public/courbe-filles.png","./public/courbe-garcons.png","./public/icon-192.png","./public/icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}))) });
