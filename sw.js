ae4060714a1d06f924489ac4e704b7a78a119c15
dauphinem <dauphine.monclar@gmail.com>
2026-07-27 18:10:52 +0200

    v2.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="grow-static-v1";
+const CACHE="grow-static-v3";
 const FILES=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./public/courbe-filles.png","./public/courbe-garcons.png","./public/icon-192.png","./public/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
